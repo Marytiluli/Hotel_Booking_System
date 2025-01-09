c87ddbb4452e46cb0e980f2ba6c9bafcdb1cbe59
marytiluli <marytiluli@gmail.com>
2025-01-09 12:05:11 +0300

    Constructor to initialize room attributes.

--- Hotel Booking System.cpp
+++ Hotel Booking System.cpp
@@ -11,3 +11,7 @@ public:
     int roomNumber; // Declare roomNumber
     string type; // Declare type
     bool isOccupied; // Declare isOccupied
+    
+    // Constructor to initialize room attributes.
+    Room(int num, string t) : roomNumber(num), type(t), isOccupied(false) {}
+};
